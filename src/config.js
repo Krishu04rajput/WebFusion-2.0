a1db489a44629bafa6a0c3127b87f9a377ca9ce3
Krishu04rajput <k.rajput0542@gmail.com>
2026-08-09 14:00:13 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export const WebFusionConfig = {
+
+    version: "2.0.0",
+
+    architecture: "x86-64",
+
+    engine: "qemu-wasm",
+
+    mode: "local",
+
+    memoryMB: 2048,
+
+    gpu: "webgpu",
+
+    debug: true
+
+};
